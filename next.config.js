e1eafb74efa8d9d505914268eedba6b2e39584dd
Mike DeVita <mike@relative.media>
2021-11-26 16:12:55 -0700

    boop

--- next.config.js
+++ next.config.js
@@ -6,6 +6,6 @@ module.exports = {
     publicRuntimeConfig: {
         apiUrl: process.env.NODE_ENV === 'development'
             ? 'http://localhost:3000/api' // development api
-            : 'http://localhost:3000/api' // production api
+            : 'https://satisfactory.ndboost.com/api' // production api
     }
 }
